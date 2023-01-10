f8d4efab2cf1b8c6e43e450023668a302e7bb058
suzzeong <su981006@naver.com>
2023-01-10 16:22:15 +0900

    add: PageItemComponent 만들기

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 import { VideoComponent } from './components/page/item/video.js';
+import { Component } from './components/component.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -14,16 +15,16 @@ class App {
       'Image Title',
       'https://picsum.photos/600/300'
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
     
     const video = new VideoComponent('Video Title', 'https://youtu.be/Lir0-QXHCwg')
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
 
     const note = new NoteComponent('Note Title', 'Note Body');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
     
   }
 }
