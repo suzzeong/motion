4344bd47c39317f9850efb6bfc17e1632d0022a9
suzzeong <su981006@naver.com>
2023-01-11 14:18:24 +0900

    add: dialog 만들기

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,8 @@
+import { InputDialog } from './components/dialog/dialog.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent, PageItemComponent } from './components/page/page.js';
+import { PageComponent, PageItemComponent, } from './components/page/page.js';
 import { VideoComponent } from './components/page/item/video.js';
 class App {
     constructor(appRoot) {
@@ -15,6 +16,17 @@ class App {
         this.page.addChild(note);
         const todo = new TodoComponent('Todo Title', 'Todo Item');
         this.page.addChild(todo);
+        const imageBtn = document.querySelector('#new-image');
+        imageBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+            dialog.setOnCloseListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.setOnSubmitListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.attachTo(document.body);
+        });
     }
 }
 new App(document.querySelector('.document'));
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
+import { InputDialog } from './components/dialog/dialog.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
+import {
+  Composable,
+  PageComponent,
+  PageItemComponent,
+} from './components/page/page.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { Component } from './components/component.js';
 
@@ -16,8 +21,11 @@ class App {
       'https://picsum.photos/600/300'
     );
     this.page.addChild(image);
-    
-    const video = new VideoComponent('Video Title', 'https://youtu.be/Lir0-QXHCwg')
+
+    const video = new VideoComponent(
+      'Video Title',
+      'https://youtu.be/Lir0-QXHCwg'
+    );
     this.page.addChild(video);
 
     const note = new NoteComponent('Note Title', 'Note Body');
@@ -25,7 +33,20 @@ class App {
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
     this.page.addChild(todo);
-    
+
+    const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+    imageBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어서 페이지에 추가해준다
+        dialog.removeFrom(document.body);
+      });
+      dialog.attachTo(document.body);
+    });
   }
 }
 
